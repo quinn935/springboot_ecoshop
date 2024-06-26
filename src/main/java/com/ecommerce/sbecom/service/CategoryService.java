3f5b1be164fdfddbf206e0ac8564cb78233ac037
Quinn <yue.jones@coverwhale.com>
2024-06-26 01:20:09 -0500

    "first init"

---
+++ src/main/java/com/ecommerce/sbecom/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.ecommerce.sbecom.service;
+
+import com.ecommerce.sbecom.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+
+    void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category, Long categoryId);
+}
